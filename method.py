2ffb66457b43036115b17cac07c42a91540119ba
K1RUN <syzdykov.nurgalym@gmail.com>
2024-03-11 00:51:43 +0300

    Butcher tableau parsing done, rk method is working (need to test)

---
+++ method.py
@@ -0,0 +1,62 @@
+from typing import Callable
+import fractions as fr
+import math
+
+
+def read_butcher_tableau() -> dict:
+    filename = input()
+    with open(filename) as file:
+        info = file.read().splitlines()
+
+    def is_valid_number(s: str):
+        """CHECK IF STRING CONTAINS A VALID NUMBER: POSITIVE OR NEGATIVE RATIONAL (WITH '/' SYMBOL) OR INTEGER"""
+        values = s.split('/')
+        return (len(values) == 1 or len(values) == 2) and all(num.lstrip('-+').isdigit() for num in values)
+
+    table = {}
+
+    for i, line in enumerate(info):
+        """LAST LINE IN INFO STANDS FOR b COEFFICIENTS, b COEFFICIENTS WILL BE ADDED SEPARATELY AFTER THE CYCLE"""
+        """https://en.wikipedia.org/wiki/List_of_Runge%E2%80%93Kutta_methods#Implicit_Runge%E2%80%93Kutta_methods"""
+        row = line.split()
+        if all(is_valid_number(string) for string in row):
+            row = [float(fr.Fraction(number)) for number in row]
+            if i != len(info) - 1:
+                table.setdefault('c_', []).append(row[0])
+                table.setdefault('a_', []).append(row[1:])
+
+            elif i == len(info) - 1:
+                table.setdefault('b_', []).extend(row)
+
+    return table
+
+
+tableau = read_butcher_tableau()
+
+
+def get_k_coefficients(t: float, y: float, h: float, f: Callable[[float, float], float]) \
+        -> list[float]:
+    a_ = tableau['a_']
+    c_ = tableau['c_']
+    k = []
+    t_n = t
+    y_n = y
+    for i in range(len(a_)):
+        for j in range(len(a_[0])):
+            if not math.fabs(a_[i][j] - 0) < 1e-10:
+                y_n += h * a_[i][j] * k[j - 1]
+        t_n += c_[i] * h
+        k.append(f(t_n, y_n))
+        t_n = t
+        y_n = y
+    return k
+
+
+def rk_one_step(t: float, y: float, h: float, f: Callable[[float, float], float]) -> tuple[float, float]:
+    k = get_k_coefficients(t, y, h, f)
+    y_n = y
+    b_ = tableau['b_']
+    for i in range(len(b_)):
+        y_n += h * b_[i] * k[i]
+    t_n = t + h
+    return t_n, y_n
---
+++ test.py
@@ -0,0 +1,61 @@
+import matplotlib.pyplot as plt
+from method import *
+import numpy as np
+
+
+def f(x, y):
+    return x**2
+
+
+def y1(x):
+    return x**3/3. + 2/3.
+
+
+x0 = 1.
+y0 = 1.
+dx = 25
+x_end = 500
+
+x_rk = [x0]
+y_rk = [y0]
+y = y0
+x = x0
+
+while x <= x_end:
+    x, y = rk_one_step(x, y, dx, f)
+    x_rk.append(x)
+    y_rk.append(y)
+
+
+def Euler(x, y, dx, dydx):
+    return x + dx, y + dx * dydx(x, y)
+
+
+x_eu = [x0]
+y_eu = [y0]
+
+y = y0
+x = x0
+
+while x <= x_end:
+    x, y = Euler(x, y, dx, f)
+
+    x_eu.append(x)
+    y_eu.append(y)
+
+
+plt.figure(figsize=(7,5))
+
+plt.plot(np.linspace(1,500,500), y1(np.linspace(1,500,500)),
+         label="Analytical solution",color="red", lw=2)
+
+plt.plot(x_rk, y_rk, label="Numerical solution:\nRunge-Kutta", dashes=(3,2), color="blue",
+        lw=3)
+plt.plot(x_eu, y_eu, label="Numerical solution:\nEuler", dashes=(3,2), color="green",
+        lw=3)
+
+plt.legend(loc="best", fontsize=12)
+plt.title(r"Solution to ODE: $\quad\frac{dy}{dx}=x^2$")
+plt.xlabel("x", fontsize=12)
+plt.ylabel("y", fontsize=12)
+plt.show()
